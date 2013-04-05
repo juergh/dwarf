bae7cdde4dc97e9c04cc76037a9ef9a0dd3970d6
Juerg Haefliger <juerg.haefliger@hp.com>
2013-04-05 12:28:43 +0200

    rename bin/dwarfclient to bin/dwarfcli to avoid namespace clashes

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name='python-dwarf',
     version='0.0.1',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    scripts=['bin/dwarf'],
+    scripts=['bin/dwarfcli'],
 )
