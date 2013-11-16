c26e8bad19a64cf8b3ee4c3598770fdb1e74765a
Juerg Haefliger <juerg.haefliger@hp.com>
2013-11-16 13:38:51 +0100

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dwarf',
-    version='0.0.1',
+    version='0.1.0',
     summary='OpenStack API on top of libvirt/kvm',
     author='Juerg Haefliger <juergh@gmail.com>',
 
