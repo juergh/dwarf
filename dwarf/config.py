9392fa7b62296f8143492a3a9d08dbd02dbc0275
Juerg Haefliger <juerg.haefliger@hp.com>
2014-03-04 10:54:10 +0100

    move the base config to a dict

--- dwarf/config.py
+++ dwarf/config.py
@@ -24,6 +24,17 @@ import yaml
 
 LOG = logging.getLogger(__name__)
 
+_BASE_CONFIG = {
+    # Directories
+    'instances_dir': '/var/lib/dwarf/instances',
+    'instances_base_dir': '/var/lib/dwarf/instances/_base',
+    'images_dir': '/var/lib/dwarf/images',
+
+    # Database and logfile
+    'dwarf_db': '/var/lib/dwarf/dwarf.db',
+    'dwarf_log': '/var/lib/dwarf/dwarf.log',
+}
+
 
 class _Config(object):
 
@@ -32,7 +43,6 @@ class _Config(object):
         cfile = os.path.join(os.path.dirname(__file__), '../etc', 'dwarf.conf')
         if not os.path.exists(cfile):
             cfile = '/etc/dwarf.conf'
-
         with open(cfile, 'r') as fh:
             cfg = yaml.load(fh)
 
@@ -40,16 +50,9 @@ class _Config(object):
         if cfg is None:
             cfg = {}
 
-        # Add base environment information
-        cfg['dwarf_dir'] = '/var/lib/dwarf'
-
-        cfg['run_dir'] = os.path.join(cfg['dwarf_dir'], 'run')
-        cfg['instances_dir'] = os.path.join(cfg['dwarf_dir'], 'instances')
-        cfg['instances_base_dir'] = os.path.join(cfg['instances_dir'], '_base')
-        cfg['images_dir'] = os.path.join(cfg['dwarf_dir'], 'images')
-
-        cfg['dwarf_db'] = os.path.join(cfg['dwarf_dir'], 'dwarf.db')
-        cfg['dwarf_log'] = os.path.join(cfg['dwarf_dir'], 'dwarf.log')
+        # Add base config information
+        for (key, val) in _BASE_CONFIG.iteritems():
+            cfg[key] = val
 
         # Add the config data as attributes to our object
         for (key, val) in cfg.iteritems():
